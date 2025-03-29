2355809ef8b2aee307ddd2d71accea5b34d20cdd
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-03-29 18:53:08 +0530

    Today's practice

---
+++ JavaScript/Lecture23/control.js
@@ -0,0 +1,12 @@
+console.log('Learning about comparison operator in javaScript');
+console.log(5==5);
+
+console.log('program start');
+let age=4;
+if(age>=18){
+  console.log('You are an adult');
+}
+else{
+  console.log('You are minor');
+}
+console.log('Program end');
