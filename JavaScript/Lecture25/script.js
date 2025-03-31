45a6770af62cafeac98c6a1c7ebdcb7e75c828c1
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-03-31 21:26:56 +0530

    today's practice

---
+++ JavaScript/Lecture25/script.js
@@ -0,0 +1,19 @@
+let str="Hello";
+console.log(str);
+
+let str1="Everyone";
+console.log(str1);
+
+let str2=`Dear sir
+
+Thank ypu
+
+yours faithfully`;
+console.log(str2);
+
+let bossName=`John`;
+let str3=`Dear `+ bossName+` Thankyou
+
+so much
+for help me alot`;
+console.log(str3);
