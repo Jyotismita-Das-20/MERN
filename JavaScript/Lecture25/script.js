14f5c11182fba442c3114771393772f04b80010b
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-04-01 10:29:43 +0530

    Today's practice

--- JavaScript/Lecture25/script.js
+++ JavaScript/Lecture25/script.js
@@ -12,8 +12,57 @@ yours faithfully`;
 console.log(str2);
 
 let bossName=`John`;
-let str3=`Dear `+ bossName+` Thankyou
+let str3=`Dear ${bossName}, Thankyou
 
 so much
 for help me alot`;
 console.log(str3);
+
+let num=5;
+let result;
+if(num%2==0){
+  result='Even';
+}else{
+  result='odd';
+}
+console.log(`The number is ${result}.`);
+
+//Guard operator
+let username='';
+let defaultUsername='Guest';
+let displayName=username||defaultUsername;
+console.log(displayName);
+
+//Default operators
+let userAge=null;
+let defaultAge=18;
+let ageToDisplay=userAge??defaultAge;
+console.log(ageToDisplay);
+
+let day=2;
+switch(day){
+  case 1:
+    day="Monday";
+    break;
+  case 2:
+    day="Tuesday";
+    break;
+  case 3:
+    day="Wednesday";
+    break;
+  case 4:
+    day="Thursday";
+    break;
+  case 5:
+    day="Friday";
+    break;
+  case 6:
+    day="Saturday";
+    break;
+  case 7:
+    day="Sunday";
+    break;
+  default:
+    day="Invalid day";
+}
+console.log(day);
