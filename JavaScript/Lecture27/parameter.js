d1175db98238f34ea468cf64df6ed83a4447599f
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-04-07 16:25:38 +0530

    Today's Practice

--- JavaScript/Lecture27/parameter.js
+++ JavaScript/Lecture27/parameter.js
@@ -4,3 +4,29 @@ function getSum(num1,num2){
 }
 
 console.log(getSum(2,3));
+
+function sum(num1, num2){
+  let add=num1+num2;
+  return add;
+}
+
+let x=10,y=5;
+console.log(`x: ${x}, y: ${y}`);
+console.log(sum(x,y));
+console.log(`x: ${x}, y: ${y}`);
+
+function sum1(num3, num4){
+  num3+=num4;
+  return num3;
+}
+
+let p=10,q=5;
+console.log(`x: ${p}, y: ${q}`);
+console.log(sum1(p,q));
+console.log(`x: ${p}, y: ${q}`);
+
+let i=0;
+while(i<5){
+  console.log(i);
+  i++;
+}
