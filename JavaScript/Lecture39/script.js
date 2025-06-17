0a3a75699510a9e4674be6f459586cbc669b7f72
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-06-17 22:02:28 +0530

    Today's practice

---
+++ JavaScript/Lecture39/script.js
@@ -0,0 +1,16 @@
+function hello(){
+  return 'Hello World';
+}
+
+let str = hello();
+console.log(str);
+console.log(typeof str);
+
+async function ola() {
+  return 'Ola Mundo';
+}
+
+let str2 = ola();
+console.log(str2);
+console.log(typeof str2);
+str2.then(data => console.log(data));
