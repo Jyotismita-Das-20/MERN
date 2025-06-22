79509a5d09554e452adff1a95590c1ef736c2701
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-06-22 21:00:44 +0530

    Start learning React library with Prashant sir

---
+++ 3_React/4-first-component/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Heading from './components/heading'
+import Paragraph from './components/paragraph'
+
+function App() {
+
+
+  return (
+    <>
+      <Heading/>
+      <Paragraph/>
+
+      <button>Click here</button>
+    </>
+  )
+}
+
+export default App
---
+++ 3_React/4-first-component/src/components/paragraph.jsx
@@ -0,0 +1,5 @@
+function Paragraph() {
+  return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ab earum sed laborum ex iure vel perspiciatis molestiae reiciendis aut ratione, ipsa doloremque dolor tempore quidem, nulla porro fuga error!</p>;
+}
+
+export default Paragraph;
