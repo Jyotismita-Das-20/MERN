0a104d4731af59fcd78c7c6ea6eee78188127024
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-06-24 18:03:37 +0530

    Today's Practice

--- 3_React/4-first-component/src/App.jsx
+++ 3_React/4-first-component/src/App.jsx
@@ -5,11 +5,16 @@ import {DangerButton, SuccessButton} from './components/Button'
 import DynamicComponent from './components/DynamicComponent'
 import StudentList from './components/StudentList'
 import RandomNumber from './components/RandomNumber'
+import Title from './components/Title'
 
 function App() {
 
   return (
     <>
+      <Title titleText = "Hello World"/>
+      <Title titleText = "KG coding"/>
+      <Title titleText = "Subscribe"/>
+      <Title titleText = "Learning Props"/>
       <RandomNumber />
       <RandomNumber />
       <RandomNumber />
