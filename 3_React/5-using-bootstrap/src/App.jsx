8e477354a08e32b52456098eb5ba43537b3f7eab
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-06-29 21:41:59 +0530

    Practice

---
+++ 3_React/5-using-bootstrap/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="my-text">This is a piece of text</p>
+      <p className="text-primary bg-success-subtle mb-5 fw-lighter w-25">This is a piece of text</p>
+      <p className="text-primary bg-success-subtle">This is a piece of text</p>
+    </>
+  )
+}
+
+export default App
