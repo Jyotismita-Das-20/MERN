367f24fc11d8914d4b7ca866fa44d5bdc1362a01
Jyotismita Das <jyotismitadas68@gmail.com>
2025-06-30 17:34:30 +0530

    today's practice

--- 3_React/5-using-bootstrap/src/App.jsx
+++ 3_React/5-using-bootstrap/src/App.jsx
@@ -1,3 +1,4 @@
+import {Camera, Trash2} from 'lucide-react';
 function App() {
 
   return (
@@ -5,6 +6,14 @@ function App() {
       <p className="my-text">This is a piece of text</p>
       <p className="text-primary bg-success-subtle mb-5 fw-lighter w-25">This is a piece of text</p>
       <p className="text-primary bg-success-subtle">This is a piece of text</p>
+
+      <ul className="list-group m-3 list-group-numbered">
+        <li className="list-group-item">Home</li>
+        <li className="list-group-item active">Contact</li>
+        <li className="list-group-item disabled shadow-lg">Product</li>
+      </ul><br />
+      <Camera color='red' size={72}/>
+      <Trash2 color='red' size={72}/>
     </>
   )
 }
