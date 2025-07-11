992e1623f37ddf6eec41f3fecbbf1d34447e95f5
Jyotismita Das <jyotismitadas68@gmail.com>
2025-07-11 18:09:41 +0530

    Learn how to work in both tailwind and vite react at a time

--- 3_React/6-learning-map/src/App.jsx
+++ 3_React/6-learning-map/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todoItems = [
+    {id:1, todoText: "Buy milk", todoDate: "4-sept-2023"},
+    {id:2, todoText: "Go to College", todoDate: "Weekday"},
+    {id:3, todoText: "Exercise", todoDate: "Everyday"},
+  ];
+
+  function convertObjectToTodoText(todoItem) {
+    console.log(`Convert function was called with item ${todoItem}, converting this to ${todoItem.todoText}`);
+    return todoItem.todoText;
+  }
+
+  console.log("----------start----------");
+  todoItems.forEach(item => {
+    const str = convertObjectToTodoText(item);
+    console.log(str);
+  })
+  console.log("----------end----------");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="text-red-500 bg-green-800">Learning Map</div>
   )
 }
 
