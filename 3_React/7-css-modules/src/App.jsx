4e59f7dbe6b43cef1bb62537da916ea5141e1f32
Jyotismita Das <jyotismitadas68@gmail.com>
2025-07-13 21:04:03 +0530

    P

--- 3_React/7-css-modules/src/App.jsx
+++ 3_React/7-css-modules/src/App.jsx
@@ -1,9 +1,18 @@
+import Container from './Container'
+import style from './App.module.css'
+
 function App() {
 
   return (
     <>
-      <h1 className="heading">Learning css modules</h1>
+      <Container>
+        <h1 className={style.heading}>Learning css modules</h1>
+        <p className={`${style.para} ${style.heading}`}>This is a sample paragraph</p>
+      </Container>
+
+      <Container>This is another random text</Container>
     </>
   )
 }
-export default App
+
+export default App;
